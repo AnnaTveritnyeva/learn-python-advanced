c72d9acca498f520efed0543b28697d7beb5fcc3
AnnaTveritnyeva <atveretniv@gmail.com>
2023-02-11 12:46:54 +0200

    Teacher's solution to closure exercise

--- closure/closure.py
+++ closure/closure.py
@@ -1,9 +1,9 @@
 def get_filter_by(num):
     def divide_all(container):
         divided_by_num = []
-        for i in range(len(container)):
-            if container[i] % num == 0:
-                divided_by_num.append(container[i])
+        for i in container:
+            if i % num == 0:
+                divided_by_num.append(i)
         return divided_by_num
 
     return divide_all
