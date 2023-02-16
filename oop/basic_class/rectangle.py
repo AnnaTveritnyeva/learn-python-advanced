18f4b73912dbdec4dbafbcfdc40ec31cf86c8d02
AnnaTveritnyeva <atveretniv@gmail.com>
2023-02-16 14:55:03 +0200

    Teacher's solution to basic_class exercise

--- oop/basic_class/rectangle.py
+++ oop/basic_class/rectangle.py
@@ -3,9 +3,11 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-    def set_size(self, width, height):
-        self.__width = width
-        self.__height = height
+    def set_size(self, new_width, new_height):
+        if new_width > 0:
+            self.__width = new_width
+        if new_height > 0:
+            self.__height = new_height
 
     def print(self):
         for h in range(self.__height):
--- oop/basic_class/test_rectangle.py
+++ oop/basic_class/test_rectangle.py
@@ -2,3 +2,11 @@ from rectangle import Rectangle
 
 rect = Rectangle(12, 4)
 rect.print()
+print()
+
+rect.set_size(10, 5)
+rect.print()
+print()
+
+rect.set_size(0, 5)
+rect.print()
