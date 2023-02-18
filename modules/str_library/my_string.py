f0a8fc97228c2a0388b2c2ccd42d397d93676316
AnnaTveritnyeva <atveretniv@gmail.com>
2023-02-18 10:28:06 +0200

    My solution to modules exercise

---
+++ modules/str_library/my_string.py
@@ -0,0 +1,9 @@
+class MyString:
+    def __init__(self, text=""):
+        self.__text = text
+
+    def __str__(self):
+        return self.__text
+
+    def __add__(self, other):
+        return MyString(self.__text + '-' + other.__text)
---
+++ modules/test_string.py
@@ -0,0 +1,7 @@
+from str_library.my_string import MyString
+
+s1 = MyString("I")
+s2 = MyString("Love")
+s3 = MyString("Python")
+s4 = s1 + s2 + s3
+print(s4)
